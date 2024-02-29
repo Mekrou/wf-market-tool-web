450e83ecd7a3d18f958f325810f6d4c480262da0
Mekrou <mills.ryan.011@gmail.com>
2024-02-29 17:43:36 -0500

    successfully added an order to the site

--- app.js
+++ app.js
@@ -12,6 +12,24 @@ async function login() {
     return await wfMarketReq.post('/auth/signin', requestBody);
 }
 
+async function createOrder(responseLogin) {
+    const requestBody = {
+        "item": "5a2feeb1c2c9e90cbdaa23d2",
+        "order_type": "sell",
+        "platinum": 12,
+        "quantity": 5,
+        "visible": true,
+    }
+
+    console.log(responseLogin.headers.authorization)
+
+    return await axios.post('https://api.warframe.market/v1/profile/orders', requestBody, {
+        headers: {
+            'Authorization': responseLogin.headers.authorization,
+        }
+    })
+}
+
 async function main() {
     wfMarketReq = axios.create({
         baseURL: 'https://api.warframe.market/v1',
@@ -19,13 +37,16 @@ async function main() {
             'Authorization': 'JWT ' + await credentials.readFromCredentials(credentials.CredentialKey.token),
             'Content-Type': 'application/json',
             'platform': 'pc',
-            'language': 'en'
+            'language': 'en',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0',
         }
     })
 
 
-    const response = await login();
-    console.log(response.data);
+    const responseLogin = await login();
+
+    const responseOrder = await createOrder(responseLogin);
+    const order_id = responseOrder.data.payload.order.id;
 }
 
 main();
