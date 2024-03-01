70861eb2b85d50877930d0bc6f905fec28369549
Mekrou <mills.ryan.011@gmail.com>
2024-02-29 19:51:47 -0500

    implemented skeleton of button

--- app.js
+++ app.js
@@ -47,6 +47,16 @@ async function main() {
 
     const responseOrder = await createOrder(responseLogin);
     const order_id = responseOrder.data.payload.order.id;
+
+    setTimeout(() => {
+        console.log("CLICKING VISISBLE BUTTON!")
+        axios.put(`https://api.warframe.market/v1/profile/orders/${order_id}`, { order_id, visible: 'false'}, {
+            headers: {
+                'Authorization': responseLogin.headers.authorization,
+            }
+        })
+    }, 9000);
+
 }
 
 main();
